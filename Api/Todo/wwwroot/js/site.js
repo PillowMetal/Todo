051b4113511bcfb0a57ffe221446f62ef8650ee4
PillowMetal <steve@swaynesworld.net>
2020-04-18 13:51:22 -0600

    Cleaned up JavaScript file to comply with code quality and standards

--- Api/Todo/wwwroot/js/site.js
+++ Api/Todo/wwwroot/js/site.js
@@ -1,15 +1,15 @@
-const uri = 'api/TodoItems';
+const uri = "api/TodoItems";
 let todos = [];
 
 function getItems() {
     fetch(uri)
         .then(response => response.json())
         .then(data => _displayItems(data))
-        .catch(error => console.error('Unable to get items.', error));
+        .catch(error => console.error("Unable to get items.", error));
 }
 
 function addItem() {
-    const addNameTextbox = document.getElementById('add-name');
+    const addNameTextbox = document.getElementById("add-name");
 
     const item = {
         isComplete: false,
@@ -17,56 +17,56 @@ function addItem() {
     };
 
     fetch(uri, {
-        method: 'POST',
+        method: "POST",
         headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
+            "Accept": "application/json",
+            "Content-Type": "application/json"
         },
         body: JSON.stringify(item)
     })
         .then(response => response.json())
         .then(() => {
             getItems();
-            addNameTextbox.value = '';
+            addNameTextbox.value = "";
         })
-        .catch(error => console.error('Unable to add item.', error));
+        .catch(error => console.error("Unable to add item.", error));
 }
 
 function deleteItem(id) {
     fetch(`${uri}/${id}`, {
-        method: 'DELETE'
+        method: "DELETE"
     })
         .then(() => getItems())
-        .catch(error => console.error('Unable to delete item.', error));
+        .catch(error => console.error("Unable to delete item.", error));
 }
 
 function displayEditForm(id) {
-    const item = todos.find(item => item.id === id);
+    const item = todos.find(t => t.id === id);
 
-    document.getElementById('edit-name').value = item.name;
-    document.getElementById('edit-id').value = item.id;
-    document.getElementById('edit-isComplete').checked = item.isComplete;
-    document.getElementById('editForm').style.display = 'block';
+    document.getElementById("edit-name").value = item.name;
+    document.getElementById("edit-id").value = item.id;
+    document.getElementById("edit-isComplete").checked = item.isComplete;
+    document.getElementById("editForm").style.display = "block";
 }
 
 function updateItem() {
-    const itemId = document.getElementById('edit-id').value;
+    const itemId = document.getElementById("edit-id").value;
     const item = {
         id: parseInt(itemId, 10),
-        isComplete: document.getElementById('edit-isComplete').checked,
-        name: document.getElementById('edit-name').value.trim()
+        isComplete: document.getElementById("edit-isComplete").checked,
+        name: document.getElementById("edit-name").value.trim()
     };
 
     fetch(`${uri}/${itemId}`, {
-        method: 'PUT',
+        method: "PUT",
         headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
+            "Accept": "application/json",
+            "Content-Type": "application/json"
         },
         body: JSON.stringify(item)
     })
         .then(() => getItems())
-        .catch(error => console.error('Unable to update item.', error));
+        .catch(error => console.error("Unable to update item.", error));
 
     closeInput();
 
@@ -74,50 +74,50 @@ function updateItem() {
 }
 
 function closeInput() {
-    document.getElementById('editForm').style.display = 'none';
+    document.getElementById("editForm").style.display = "none";
 }
 
 function _displayCount(itemCount) {
-    const name = (itemCount === 1) ? 'to-do' : 'to-dos';
+    const name = (itemCount === 1) ? "to-do" : "to-dos";
 
-    document.getElementById('counter').innerText = `${itemCount} ${name}`;
+    document.getElementById("counter").innerText = `${itemCount} ${name}`;
 }
 
 function _displayItems(data) {
-    const tBody = document.getElementById('todos');
-    tBody.innerHTML = '';
+    const tBody = document.getElementById("todos");
+    tBody.innerHTML = "";
 
     _displayCount(data.length);
 
-    const button = document.createElement('button');
+    const button = document.createElement("button");
 
     data.forEach(item => {
-        let isCompleteCheckbox = document.createElement('input');
-        isCompleteCheckbox.type = 'checkbox';
+        const isCompleteCheckbox = document.createElement("input");
+        isCompleteCheckbox.type = "checkbox";
         isCompleteCheckbox.disabled = true;
         isCompleteCheckbox.checked = item.isComplete;
 
-        let editButton = button.cloneNode(false);
-        editButton.innerText = 'Edit';
-        editButton.setAttribute('onclick', `displayEditForm(${item.id})`);
+        const editButton = button.cloneNode(false);
+        editButton.innerText = "Edit";
+        editButton.setAttribute("onclick", `displayEditForm(${item.id})`);
 
-        let deleteButton = button.cloneNode(false);
-        deleteButton.innerText = 'Delete';
-        deleteButton.setAttribute('onclick', `deleteItem(${item.id})`);
+        const deleteButton = button.cloneNode(false);
+        deleteButton.innerText = "Delete";
+        deleteButton.setAttribute("onclick", `deleteItem(${item.id})`);
 
-        let tr = tBody.insertRow();
+        const tr = tBody.insertRow();
 
-        let td1 = tr.insertCell(0);
+        const td1 = tr.insertCell(0);
         td1.appendChild(isCompleteCheckbox);
 
-        let td2 = tr.insertCell(1);
-        let textNode = document.createTextNode(item.name);
+        const td2 = tr.insertCell(1);
+        const textNode = document.createTextNode(item.name);
         td2.appendChild(textNode);
 
-        let td3 = tr.insertCell(2);
+        const td3 = tr.insertCell(2);
         td3.appendChild(editButton);
 
-        let td4 = tr.insertCell(3);
+        const td4 = tr.insertCell(3);
         td4.appendChild(deleteButton);
     });
 
