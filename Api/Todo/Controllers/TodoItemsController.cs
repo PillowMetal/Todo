f152d46c707a2762c3810d647af16abd7782f177
Steve Swayne <steve.swayne@schools.utah.gov>
2020-04-18 12:47:40 -0600

    Added initial set of source files used

---
+++ Api/Todo/Controllers/TodoItemsController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Todo.Models;
+using static System.Boolean;
+using static System.String;
+
+namespace Todo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItemsController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoItemsController(TodoContext context) => _context = context;
+
+        [HttpGet]
+        [HttpHead]
+        public async Task<ActionResult<IEnumerable<TodoItemDto>>> GetTodoItems(string isComplete) => IsNullOrWhiteSpace(isComplete)
+            ? await _context.TodoItems.Select(t => ItemToDto(t)).ToListAsync()
+            : !TryParse(isComplete.Trim(), out bool flag)
+                ? (ActionResult<IEnumerable<TodoItemDto>>)BadRequest()
+                : await _context.TodoItems.Where(t => t.IsComplete == flag).Select(t => ItemToDto(t)).ToListAsync();
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItemDto>> GetTodoItem(long id)
+        {
+            TodoItem todoItem = await _context.TodoItems.FindAsync(id);
+            return todoItem == null ? (ActionResult<TodoItemDto>)NotFound() : ItemToDto(todoItem);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id, TodoItemDto todoItemDto)
+        {
+            if (id != todoItemDto.Id)
+                return BadRequest();
+
+            TodoItem todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+                return NotFound();
+
+            todoItem.Name = todoItemDto.Name;
+            todoItem.IsComplete = todoItemDto.IsComplete;
+
+            try
+            {
+                _ = await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException) when (!TodoItemExists(id))
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TodoItemDto>> PostTodoItem(TodoItem todoItemDto)
+        {
+            var todoItem = new TodoItem
+            {
+                IsComplete = todoItemDto.IsComplete,
+                Name = todoItemDto.Name
+            };
+
+            _ = _context.TodoItems.Add(todoItem);
+            _ = await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, ItemToDto(todoItem));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<TodoItem>> DeleteTodoItem(long id)
+        {
+            TodoItem todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+                return NotFound();
+
+            _ = _context.TodoItems.Remove(todoItem);
+            _ = await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TodoItemExists(long id) => _context.TodoItems.Any(e => e.Id == id);
+
+        private static TodoItemDto ItemToDto(TodoItem todoItem) => new TodoItemDto
+        {
+            Id = todoItem.Id,
+            Name = todoItem.Name,
+            IsComplete = todoItem.IsComplete
+        };
+    }
+}
---
+++ Api/Todo/Startup.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Todo.Models;
+
+namespace Todo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            _ = services.AddDbContext<TodoContext>(options => options.UseInMemoryDatabase("TodoList"));
+            _ = services.AddControllers(options => options.ReturnHttpNotAcceptable = true).AddXmlDataContractSerializerFormatters();
+        }
+
+        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+                _ = app.UseDeveloperExceptionPage();
+
+            _ = app.UseDefaultFiles();
+            _ = app.UseStaticFiles();
+
+            _ = app.UseHttpsRedirection();
+
+            _ = app.UseRouting();
+
+            _ = app.UseAuthorization();
+
+            _ = app.UseEndpoints(endpoints => endpoints.MapControllers());
+        }
+    }
+}
