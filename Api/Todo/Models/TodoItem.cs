9df83a70d6d4b092f715090abe0ef4cfa4e6991d
PillowMetal <steve@swaynesworld.net>
2020-04-20 22:07:06 -0600

    Added data annotations to the DB model and the DTO model for validation

--- Api/Todo/Models/TodoItem.cs
+++ Api/Todo/Models/TodoItem.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Todo.Models
 {
     public class TodoItem
     {
         public long Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
         public string Project { get; set; }
         public string Context { get; set; }
         public bool IsComplete { get; set; }
--- Api/Todo/Models/TodoItemCreateDto.cs
+++ Api/Todo/Models/TodoItemCreateDto.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Todo.Models
 {
     public class TodoItemCreateDto
     {
+        [Required]
         public string Name { get; set; }
+
         public string Project { get; set; }
         public string Context { get; set; }
         public bool IsComplete { get; set; }
