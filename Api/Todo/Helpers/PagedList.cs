7e148c7fbae30137129b904bfbd64cc73d5b47c4
PillowMetal <steve@swaynesworld.net>
2020-04-29 17:42:20 -0600

    Created a new custom PageList class that will contain the state of paging for any given request Included constructor and factory method to take the items, count, page size, and current page to set the internal properties of this class Included CurrentPage, TotalPages, PageSize, TotalCount, and booleans with lambdas to determine if there are previous or next pages

---
+++ Api/Todo/Helpers/PagedList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Todo.Helpers
+{
+    public class PagedList<T> : List<T>
+    {
+        public int CurrentPage { get; }
+        public int TotalPages { get; }
+        public int PageSize { get; }
+        public int TotalCount { get; }
+        public bool HasPrevious => CurrentPage > 1;
+        public bool HasNext => CurrentPage < TotalPages;
+
+        public PagedList(IEnumerable<T> items, int count, int pageNumber, int pageSize)
+        {
+            TotalCount = count;
+            PageSize = pageSize;
+            CurrentPage = pageNumber;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            AddRange(items);
+        }
+
+        public static PagedList<T> Create(List<T> source, int pageNumber, int pageSize) =>
+            new PagedList<T>(source.Skip((pageNumber - 1) * pageSize).Take(pageSize), source.Count, pageNumber, pageSize);
+    }
+}
