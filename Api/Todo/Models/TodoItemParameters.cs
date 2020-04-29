7d697b56e7285959fa9fc0e498b49f56f597b877
PillowMetal <steve@swaynesworld.net>
2020-04-29 17:39:17 -0600

    Added properties to the parameters DTO to include page number and page size in preparation for implementation of paging in the API Defaulted the page number properties to 1, and used a custom setter on the page size to limit it to a maximum size if a larger number is given

--- Api/Todo/Models/TodoItemParameters.cs
+++ Api/Todo/Models/TodoItemParameters.cs
@@ -2,7 +2,13 @@ namespace Todo.Models
 {
     public class TodoItemParameters
     {
+        private const int MaximumPageSize = 10;
+
         public string IsComplete { get; set; }
         public string SearchQuery { get; set; }
+        public int PageNumber { get; set; } = 1;
+
+        private int _pageSize = 4;
+        public int PageSize { get => _pageSize; set => _pageSize = value > MaximumPageSize ? MaximumPageSize : value; }
     }
 }
