679f3ed3a95a8fe9dd7bb4d092e95be72ec46c39
PillowMetal <steve@swaynesworld.net>
2020-05-01 17:34:52 -0600

    Added a property mapping class that contains a dictionary of string/property mapping value pairs, to tie the source DTO property to the corresponding entity properties which can be different and even numerous Added a constructor that takes the dictionary that is to be stored in the instance of the class Created an empty interface so source and destination template arguments can be used with this class

---
+++ Api/Todo/Services/PropertyMapping.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Todo.Services
+{
+    [SuppressMessage("ReSharper", "UnusedTypeParameter")]
+    public class PropertyMapping<TSource, TDestination> : IPropertyMapping
+    {
+        public Dictionary<string, PropertyMappingValue> MappingDictionary { get; set; }
+
+        public PropertyMapping(Dictionary<string, PropertyMappingValue> dictionary) => MappingDictionary = dictionary;
+    }
+}
