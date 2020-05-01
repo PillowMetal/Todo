49699868f33e94238cd64cf257e0e9df5c62aa22
PillowMetal <steve@swaynesworld.net>
2020-05-01 17:30:56 -0600

    Created a property mapping value class that contains a list of strings denoting destination properties in the entities vs the DTOs, and a revert flag to indicate that descending order should be used

---
+++ Api/Todo/Services/PropertyMappingValue.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Todo.Services
+{
+    public class PropertyMappingValue
+    {
+        public IEnumerable<string> DestinationProperties { get; }
+        public bool Revert { get; }
+
+        public PropertyMappingValue(IEnumerable<string> properties, bool revert = false)
+        {
+            DestinationProperties = properties;
+            Revert = revert;
+        }
+    }
+}
