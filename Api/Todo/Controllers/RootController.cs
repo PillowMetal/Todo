e339a236ea99a040834ccfdb253aa4a7e956998c
PillowMetal <steve@swaynesworld.net>
2020-05-06 16:41:23 -0600

    Added new controller for the Root API level that provides links and allows self-discoverability for what actions on the API are allowed

---
+++ Api/Todo/Controllers/RootController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Todo.Models;
+
+namespace Todo.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class RootController : ControllerBase
+    {
+        [HttpGet(Name = "GetRoot")]
+        public ActionResult<IEnumerable<LinkDto>> GetRoot() => new List<LinkDto>
+        {
+            new LinkDto(Url.Link("GetRoot", new { }), "self", "GET"),
+            new LinkDto(Url.Link("GetTodoItems", new { }), "get-todoitems", "GET"),
+            new LinkDto(Url.Link("PostTodoItem", new { }), "post-todoitem", "POST")
+        };
+    }
+}
