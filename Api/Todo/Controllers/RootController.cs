d892cddf1c4b07343633725bbbe39f51c609fc01
PillowMetal <steve@swaynesworld.net>
2020-05-07 18:01:37 -0600

    Reordered the links in the root controller to match the order of functions in the todo items controller

--- Api/Todo/Controllers/RootController.cs
+++ Api/Todo/Controllers/RootController.cs
@@ -12,10 +12,10 @@ namespace Todo.Controllers
         public ActionResult<IEnumerable<LinkDto>> GetRoot() => new List<LinkDto>
         {
             new LinkDto(Url.Link("GetRoot", new { }), "self", "GET"),
-            new LinkDto(Url.Link("GetTodoItems", new { }), "get-todoitems", "GET"),
-            new LinkDto(Url.Link("PostTodoItem", new { }), "post-todoitem", "POST"),
+            new LinkDto(Url.Link("OptionsTodoItems", new { }), "options-todoitems", "OPTIONS"),
             new LinkDto(Url.Link("GetTodoItems", new { }), "head-todoitem", "HEAD"),
-            new LinkDto(Url.Link("OptionsTodoItems", new { }), "options-todoitems", "OPTIONS")
+            new LinkDto(Url.Link("GetTodoItems", new { }), "get-todoitems", "GET"),
+            new LinkDto(Url.Link("PostTodoItem", new { }), "post-todoitem", "POST")
         };
     }
 }
