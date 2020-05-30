5b028c461e25097ad96c166f73e4b94693541bee
PillowMetal <steve@swaynesworld.net>
2020-05-30 01:10:34 -0600

    Converted extension method to use ToLowerInvariant

--- Api/Todo/Helpers/StringExtensions.cs
+++ Api/Todo/Helpers/StringExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System.Linq;
-using static System.Globalization.CultureInfo;
 using static System.String;
 
 namespace Todo.Helpers
@@ -7,6 +6,6 @@ namespace Todo.Helpers
     public static class StringExtensions
     {
         public static string ToLowerFirstChar(this string value) =>
-            Concat(value?.Length > 0 ? value.First().ToString().ToLower(CurrentCulture) : Empty, value?.Length > 1 ? value.Substring(1) : Empty);
+            Concat(value?.Length > 0 ? value.First().ToString().ToLowerInvariant() : Empty, value?.Length > 1 ? value.Substring(1) : Empty);
     }
 }
