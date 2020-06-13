e2038215f83423d4ea4a4f3042fc9d55fd4becdf
PillowMetal <steve@swaynesworld.net>
2020-06-13 14:18:51 -0600

    Added validation to the manipulation DTO to ensure the project is different than the context

--- Api/Todo/Models/TodoItemManipulationDto.cs
+++ Api/Todo/Models/TodoItemManipulationDto.cs
@@ -1,10 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using static System.DateTime;
 
 namespace Todo.Models
 {
-    public abstract class TodoItemManipulationDto
+    public abstract class TodoItemManipulationDto : IValidatableObject
     {
         [Required]
         public string Name { get; set; }
@@ -13,5 +14,15 @@ namespace Todo.Models
         public string Context { get; set; }
         public DateTime Date { get; set; } = Today;
         public bool IsComplete { get; set; }
+
+        #region Implementation of IValidatableObject
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Project == Context)
+                yield return new ValidationResult("The provided project must be different than the context.", new[] { nameof(TodoItemManipulationDto) });
+        }
+
+        #endregion
     }
 }
