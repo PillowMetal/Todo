7a6f02590ec84baad2d5f8e06152f26aaad0fe5a
PillowMetal <steve@swaynesworld.net>
2020-06-16 15:53:19 -0600

    Formatted fnction with Concat method call

--- Api/Todo/Helpers/StringExtensions.cs
+++ Api/Todo/Helpers/StringExtensions.cs
@@ -7,7 +7,8 @@ namespace Todo.Helpers
     public static class StringExtensions
     {
         [SuppressMessage("Design", "CA1308: Replace the call to 'ToLowerInvariant' with 'ToUpperInvariant'", Justification = "<Pending>")]
-        public static string ToLowerFirstChar(this string value) =>
-            Concat(value?.Length > 0 ? value.First().ToString().ToLowerInvariant() : Empty, value?.Length > 1 ? value.Substring(1) : Empty);
+        public static string ToLowerFirstChar(this string value) => Concat(
+            value?.Length > 0 ? value.First().ToString().ToLowerInvariant() : Empty
+          , value?.Length > 1 ? value.Substring(1) : Empty);
     }
 }
