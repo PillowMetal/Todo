be25d531cca2ab33a20546c5dff684852a27df5a
PillowMetal <steve@swaynesworld.net>
2020-06-17 19:46:48 -0600

    Renamed the root controller to refelect the route name of the API

--- Api/Todo/Controllers/ApiController.cs
+++ Api/Todo/Controllers/ApiController.cs
@@ -4,14 +4,14 @@ using Todo.Models;
 
 namespace Todo.Controllers
 {
-    [Route("api")]
     [ApiController]
-    public class RootController : ControllerBase
+    [Route("api")]
+    public class ApiController : ControllerBase
     {
-        [HttpGet(Name = nameof(GetRoot))]
-        public ActionResult<IEnumerable<LinkDto>> GetRoot() => new List<LinkDto>
+        [HttpGet(Name = nameof(GetApi))]
+        public ActionResult<IEnumerable<LinkDto>> GetApi() => new List<LinkDto>
         {
-            new LinkDto(Url.Link(nameof(GetRoot), new { }), "self", "GET"),
+            new LinkDto(Url.Link(nameof(GetApi), new { }), "self", "GET"),
             new LinkDto(Url.Link("OptionsTodoItems", new { }), "options-todoitems", "OPTIONS"),
             new LinkDto(Url.Link("GetTodoItems", new { }), "head-todoitem", "HEAD"),
             new LinkDto(Url.Link("GetTodoItems", new { }), "get-todoitems", "GET"),
